9ece71fb8185e2ec03897eccca93bb0a1da3e114
MartinRenz <martinrenz@outlook.com>
2024-10-08 12:42:55 -0300

    Project setup

---
+++ src/SideMenu.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function SideMenu() {
+  console.warn("SIDE")
+  return (
+    <div style={{ display: "flex" }}>
+        <h2>Teatro Machado de Assis</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet scelerisque velit. Fusce vestibulum, justo non laoreet sollicitudin, odio enim hendrerit ipsum, sed tincidunt nisl mi ut felis. Curabitur feugiat nisl sit amet felis tincidunt, vitae accumsan magna convallis. Integer sit amet urna quis orci bibendum fermentum. Nullam egestas massa sed velit tincidunt.</p>
+    </div>
+  );
+}
+
+export default SideMenu;
