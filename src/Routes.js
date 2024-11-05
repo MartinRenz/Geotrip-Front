b6bc5cb0dfb3390f3c33f9567a4ddd2c56cd4373
MartinRenz <martinrenz@outlook.com>
2024-11-05 14:27:05 -0300

    Fixing em relação ao roteamento

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import Map from './pages/Map/Map'
 import Login from './pages/Login/Login'
 
 function RoutesApp() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/*" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/map" element={<Map />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
