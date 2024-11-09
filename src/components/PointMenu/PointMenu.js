733df572bf6c7b544d34ba396163ab5a189b2843
Eduardo-Bender <103793216+Eduardo-Bender@users.noreply.github.com>
2024-11-09 03:47:19 -0300

    imagem e ponto de menu sla

--- src/components/PointMenu/PointMenu.js
+++ src/components/PointMenu/PointMenu.js
@@ -1,17 +1,32 @@
-import './PointMenu.css'
+import './PointMenu.css';
+import placeHolder from '../../assets/icons/placeholder.jpg';
 
 function PointMenu({ point, onClose }) {
     return (
         <div className="pointMenuOverlay" onClick={onClose}>
-            <div className="pointMenu" onClick={(e) => e.stopPropagation()}>
-                <h3>{point.name}</h3>
-                <p>ID: {point.id}</p>
-                <p>Latitude: {point.latitude}</p>
-                <p>Longitude: {point.longitude}</p>
-                <button onClick={onClose}>Fechar</button>
+            <div className="pointMenuContainer">
+                <div className="pointMenuHeader" onClick={(e) => e.stopPropagation()}>
+                    <button onClick={onClose} class="closeButton">✕</button>
+                    <h3 style={{ textAlign: 'center' }}>{point.name}</h3>
+                    <img src={placeHolder} alt='gato makonha'>
+                    </img>
+                    {/* <p>ID: {point.id}</p>
+                    <p>Latitude: {point.latitude}</p>
+                    <p>Longitude: {point.longitude}</p> */}
+                </div>
+                <div className="pointMenuContent">
+                    <div style={{ marginTop: '20px' }}>
+                        <h4>Additional Information</h4>
+                        <ul>
+                            {Array.from({ length: 40 }, (_, i) => (
+                                <li key={i}>Detail Item {i + 1}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
 
-export default PointMenu
+export default PointMenu;
